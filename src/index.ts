499808a80bdaf432874112964167cbb3c0f3f240
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-08-23 12:55:48 +0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,28 @@ window.Webflow.push(() => {
   greetUser(name);
   gsapHome();
 });
+
+let myTimer;
+
+$('.card').on('click', function () {
+  $('.is--current').removeClass('is--current');
+  $(this).addClass('is--current');
+  let myIndex = $(this).index();
+  $('.app_img').eq(myIndex).addClass('is--current');
+  clearInterval(myTimer);
+  runInterval();
+});
+
+function runInterval() {
+  myTimer = setInterval(function () {
+    let currentItems = $('.is--current');
+    if (currentItems.next().length > 0) {
+      currentItems.next().addClass('is--current');
+    } else {
+      $('.card').eq(0).addClass('is--current');
+      $('.app_img').eq(0).addClass('is--current');
+    }
+    currentItems.removeClass('is--current');
+  }, 5000);
+}
+runInterval();
