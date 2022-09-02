d85ec0c41a3c112c15ff255216cc70c42fb226cc
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2022-09-02 09:56:55 +0700

    save

--- src/index.ts
+++ src/index.ts
@@ -10,30 +10,30 @@ window.Webflow.push(() => {
 gsapHome();
 // motionDev();
 
-window.Webflow.push(function () {
-  var tabTimeout;
-  clearTimeout(tabTimeout);
-  tabLoop();
+// window.Webflow.push(function () {
+//   var tabTimeout;
+//   clearTimeout(tabTimeout);
+//   tabLoop();
 
-  // define loop - cycle through all tabs
-  function tabLoop() {
-    tabTimeout = setTimeout(function () {
-      var $next = $('.wrapper-link').children('.w--current:first').next();
+//   // define loop - cycle through all tabs
+//   function tabLoop() {
+//     tabTimeout = setTimeout(function () {
+//       var $next = $('.wrapper-link').children('.w--current:first').next();
 
-      if ($next.length) {
-        $next.click(); // click resets timeout, so no need for interval
-      } else {
-        $('.wrapper-link .link-card:first').click();
-      }
-    }, 3000);
-  }
+//       if ($next.length) {
+//         $next.click(); // click resets timeout, so no need for interval
+//       } else {
+//         $('.wrapper-link .link-card:first').click();
+//       }
+//     }, 3000);
+//   }
 
-  // reset timeout if a tab is clicked
-  $('.wrapper-link .link-card').click(function () {
-    clearTimeout(tabTimeout);
-    tabLoop();
-  });
-});
+//   // reset timeout if a tab is clicked
+//   $('.wrapper-link .link-card').click(function () {
+//     clearTimeout(tabTimeout);
+//     tabLoop();
+//   });
+// });
 
 // window.Webflow.push(function () {
 //   var tabTimeout;
